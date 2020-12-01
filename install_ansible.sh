71623cd8364a6a3e556da564f2d8a46a0f529d29
Pascal ROBERT <ROBERTPASCAL@users.noreply.github.com>
2020-12-01 21:53:51 +0100

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -2,7 +2,7 @@
 
 echo '[Install Ansible]'
 sudo apt update
-sudo apt install -y software-properties-common
+sudo apt install -y software-properties-common git
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible
 echo '[Install Ansible] Done'
